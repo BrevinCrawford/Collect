fa53d365df1ce0fe8319268c6196855115492f09
BrevinCrawford <70293997+BrevinCrawford@users.noreply.github.com>
2020-11-04 17:12:12 -0600

    Add files via upload

---
+++ Hashmap.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+
+public class Hashmap {
+	public static void main(String[] args) {
+		
+		HashMap<String, Integer> hashm = new HashMap<String, Integer>();
+		hashm.put("This", 2);
+		hashm.put("Good", 4);
+	}
+
+}
---
+++ Treemap.java
@@ -0,0 +1,13 @@
+import java.util.TreeMap;
+import java.util.Map;
+
+
+
+public class Treemap {
+	public static void main(String[] args) {
+	Map<String, Integer> tree = new TreeMap<>();
+	tree.put("This", 2);
+	tree.put("Good", 4);
+	}
+
+}
